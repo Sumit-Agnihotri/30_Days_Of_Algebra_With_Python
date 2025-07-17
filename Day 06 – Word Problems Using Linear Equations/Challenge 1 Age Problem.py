9d1fb9ed25beb32cced8d4cecef9b78f5934e053
Sumit Agnihotri <163757254+Sumit-Agnihotri@users.noreply.github.com>
2025-07-17 13:13:44 +0530

    Algebra

--- Day 06 – Word Problems Using Linear Equations/Challenge 1 Age Problem.py
+++ Day 06 – Word Problems Using Linear Equations/Challenge 1 Age Problem.py
@@ -1,8 +1,11 @@
+# Problem:
+# Sumit's age is 5 more than twice his brother's age. If Sumit is 25, what is his brother's age?
+
 age = int(input("Enter the age of the first person: "))
 from sympy import symbols, Eq, solve
 
-print("Let the second person's age be x.")
+print("Let the age of the brother be x.")
 x = symbols('x')
 equation = Eq(2 * x + 5, age)
 solution = solve(equation)
-print(f"The Second person's age is: {solution[0]}")
+print(f"The age of the brother is: {solution[0]}")
---
+++ Day 06 – Word Problems Using Linear Equations/Challenge 2 Money Problem.py
@@ -0,0 +1,3 @@
+# Problem:
+# A pencil costs ₹2 more than a pen. If 1 pencil and 1 pen cost ₹22 total, find the price of each.
+
