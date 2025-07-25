8f8434dbaa8f3f9311cb84c691201d01e3962d48
Sumit Agnihotri <163757254+Sumit-Agnihotri@users.noreply.github.com>
2025-07-25 08:03:11 +0530

    Algebra

---
+++ Day 14 – Solving and Graphing Algebraic Inequalities/Challenge 1 Solve simple linear inequality.py
@@ -0,0 +1,8 @@
+from sympy import symbols, solve_univariate_inequality
+from sympy import S
+
+x = symbols('x')
+inequality = x + 2 > 5
+
+solution = solve_univariate_inequality(inequality, x, relational=False, domain=S.Reals)
+print("Solution:", solution)
