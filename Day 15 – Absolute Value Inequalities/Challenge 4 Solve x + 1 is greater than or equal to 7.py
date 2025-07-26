aac03ab5a317b4d55deae762fa39e48199aaa87d
Sumit Agnihotri <163757254+Sumit-Agnihotri@users.noreply.github.com>
2025-07-26 07:53:04 +0530

    Algebra

--- Day 15 – Absolute Value Inequalities/Challenge 4 Solve x + 1 is greater than or equal to 7.py
+++ Day 15 – Absolute Value Inequalities/Challenge 4 Solve x + 1 is greater than or equal to 7.py
@@ -2,3 +2,6 @@ from sympy import Abs, S, symbols
 from sympy.solvers.inequalities import solve_univariate_inequality
 
 x = symbols('x')
+inequality = Abs(x + 1) >= 7
+solution = solve_univariate_inequality(inequality, x, relational=False, domain=S.Reals)
+print("Solution:", solution)
