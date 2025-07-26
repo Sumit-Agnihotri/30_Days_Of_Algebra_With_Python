ea6eb466ad75c55fcc268af6097de4ea590ed1a9
Sumit Agnihotri <163757254+Sumit-Agnihotri@users.noreply.github.com>
2025-07-26 07:51:14 +0530

    Algebra

---
+++ Day 15 – Absolute Value Inequalities/Challenge 2 Solve x is greater than 5.py
@@ -0,0 +1,7 @@
+from sympy import Abs, S, symbols
+from sympy.solvers.inequalities import solve_univariate_inequality
+
+x = symbols('x')
+inequality = Abs(x) > 5
+solution = solve_univariate_inequality(inequality, x, relational=False, domain=S.Reals)
+print("Solution:", solution)
