40e46c88c86f7cb0fee3bdf79bc2d3881bac0b8b
Sumit Agnihotri <163757254+Sumit-Agnihotri@users.noreply.github.com>
2025-07-26 07:54:08 +0530

    Algebra

--- Day 15 – Absolute Value Inequalities/Challenge 5 Visualize solution of x - 3 is less than 2.py
+++ Day 15 – Absolute Value Inequalities/Challenge 5 Visualize solution of x - 3 is less than 2.py
@@ -1,4 +1,14 @@
-from sympy import Abs, S, symbols
-from sympy.solvers.inequalities import solve_univariate_inequality
+import numpy as np
+import matplotlib.pyplot as plt
 
-x = symbols('x')
+x_vals = np.linspace(-5, 10, 500)
+mask = (x_vals > 1) & (x_vals < 5)
+
+plt.plot(x_vals, [0]*len(x_vals), 'k--')
+plt.fill_between(x_vals, 0, 1, where=mask, color='skyblue', label='|x - 3| < 2')
+plt.title("Solution Set for |x - 3| < 2")
+plt.yticks([])
+plt.xlabel("x")
+plt.legend()
+plt.grid()
+plt.show()
