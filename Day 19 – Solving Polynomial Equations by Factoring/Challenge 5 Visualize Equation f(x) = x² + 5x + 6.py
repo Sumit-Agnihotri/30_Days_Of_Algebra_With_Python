af4606db71892b4a2f0972cf85ed59705d372d4f
Sumit Agnihotri <163757254+Sumit-Agnihotri@users.noreply.github.com>
2025-07-30 08:22:46 +0530

    Algebra

---
+++ Day 19 – Solving Polynomial Equations by Factoring/Challenge 5 Visualize Equation f(x) = x² + 5x + 6.py
@@ -0,0 +1,19 @@
+from sympy import symbols
+import numpy as np
+import matplotlib.pyplot as plt
+from sympy import lambdify
+
+x = symbols('x')
+f = x**2 + 5*x + 6
+f_lambdified = lambdify(x, f, 'numpy')
+
+x_vals = np.linspace(-10, 2, 400)
+y_vals = f_lambdified(x_vals)
+
+plt.plot(x_vals, y_vals, label='f(x) = x² + 5x + 6')
+plt.axhline(0, color='black', lw=0.5)
+plt.axvline(0, color='black', lw=0.5)
+plt.title("Roots of f(x) = x² + 5x + 6")
+plt.grid()
+plt.legend()
+plt.show()
