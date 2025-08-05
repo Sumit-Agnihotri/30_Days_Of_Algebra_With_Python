44e2beacff6ffef4da645fd6bb1a34a1e71a567f
Sumit Agnihotri <163757254+Sumit-Agnihotri@users.noreply.github.com>
2025-08-05 12:05:12 +0530

    Algebra

--- Day 25 – Solving Quadratic Inequalities in Python/Challenge 3 Negative Coefficients.py
+++ Day 25 – Solving Quadratic Inequalities in Python/Challenge 3 Negative Coefficients.py
@@ -1,1 +1,6 @@
 from sympy import symbols, solve_univariate_inequality, S
+
+x = symbols('x')
+inequality = (-2*x**2 + 8*x - 6) >= 0
+solution = solve_univariate_inequality(inequality, x, domain=S.Reals)
+print("Solution:", solution)
