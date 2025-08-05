ad8d45d999796e1046232ec3fd16a7046da6bc39
Sumit Agnihotri <163757254+Sumit-Agnihotri@users.noreply.github.com>
2025-08-05 12:05:57 +0530

    Algebra

--- Day 25 – Solving Quadratic Inequalities in Python/Challenge 5 Word Problem with Inequality.py
+++ Day 25 – Solving Quadratic Inequalities in Python/Challenge 5 Word Problem with Inequality.py
@@ -1,1 +1,6 @@
 from sympy import symbols, solve_univariate_inequality, S
+
+x = symbols('x')
+inequality = (x*(x + 5)) > 24
+solution = solve_univariate_inequality(inequality, x, domain=S.Reals)
+print("Solution:", solution)
